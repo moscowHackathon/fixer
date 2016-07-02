018f963401b3518b8196922c9e1019d2c4e5d51f
Andrey Perepechaev <andrey.perepechaev@lazada.com>
2016-07-02 13:20:09 +0300

    first implementation

--- main.go
+++ main.go
@@ -2,21 +2,82 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"flag"
 
 	"github.com/nlopes/slack"
 )
 
 func main() {
-	api := slack.New("YOUR_TOKEN_HERE")
-	// If you set debugging, it will log all requests to the console
-	// Useful when encountering issues
-	// api.SetDebug(true)
-	groups, err := api.GetGroups(false)
+	token := flag.String("slack-token", "", "Token from slack")
+	flag.Parse()
+
+	if token == nil || *token == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	api := slack.New(*token)
+	logger := log.New(os.Stdout, "slack-bot: ", log.Lshortfile|log.LstdFlags)
+	slack.SetLogger(logger)
+	//api.SetDebug(true)
+
+	// Find the user to post as.
+	authTest, err := api.AuthTest()
 	if err != nil {
-		fmt.Printf("%s\n", err)
+		fmt.Printf("Error getting channels: %s\n", err)
 		return
 	}
-	for _, group := range groups {
-		fmt.Printf("ID: %s, Name: %s\n", group.ID, group.Name)
+
+	botID := authTest.UserID
+
+	rtm := api.NewRTM()
+	go rtm.ManageConnection()
+
+Loop:
+	for {
+		select {
+		case msg := <-rtm.IncomingEvents:
+			fmt.Print("Event Received: ")
+			switch ev := msg.Data.(type) {
+			case *slack.HelloEvent:
+			// Ignore hello
+
+			case *slack.ConnectedEvent:
+				fmt.Println("Infos:", ev.Info)
+				fmt.Println("Connection counter:", ev.ConnectionCount)
+				// Replace #general with your Channel ID
+				rtm.SendMessage(rtm.NewOutgoingMessage("Hello world", "C1NBBSKEE"))
+
+			case *slack.MessageEvent:
+				fmt.Printf("Message: %+v\n", ev)
+				if strings.Contains(ev.Text, "<@"+botID+">") == false {
+					break
+
+				}
+				rtm.SendMessage(rtm.NewOutgoingMessage("Сам дурак", ev.Channel))
+
+			case *slack.PresenceChangeEvent:
+				fmt.Printf("Presence Change: %v\n", ev)
+
+			case *slack.LatencyReport:
+				fmt.Printf("Current latency: %v\n", ev.Value)
+
+			case *slack.RTMError:
+				fmt.Printf("\033[0;31mError:\033[0m %s\n", ev.Error())
+
+			case *slack.InvalidAuthEvent:
+				fmt.Printf("Invalid credentials")
+				break Loop
+
+			default:
+
+				// Ignore other events..
+				// fmt.Printf("Unexpected: %v\n", msg.Data)
+			}
+		}
 	}
 }
